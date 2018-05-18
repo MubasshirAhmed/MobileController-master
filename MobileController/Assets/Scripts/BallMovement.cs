af41491f7ce3feacf6e7f7979b0a1891e8fa447a
MUBASSHIRAHMED\Mubasshir Ahmed <MubasshirAhmed26@gmail.com>
2018-05-18 22:17:01 +0600

    Initial commit

---
+++ MobileController/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private float moveSpeed = 25;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(VirtualJoystick.joystick.Horizontal(), 0, VirtualJoystick.joystick.Vertical()) * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ MobileController/Assets/Scripts/ButtonControlScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControlScript : MonoBehaviour
+{
+    private bool ballMove;
+    private float movementSpeed = 25;
+    public GameObject ball;
+    // Use this for initialization
+    void Start()
+    {
+        //int n = 10;
+        //string[] responses = { "Positive", "Negative" };
+        //Debug.Log((n >> 31) & 1);
+        //Debug.Log((n ^ (n >> 31)) - (n >> 31));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballMove)
+            ball.transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0, 0));
+    }
+
+    public void OnButtonDown(bool site)
+    {
+        ballMove = true;
+        if (site)
+            movementSpeed = -Mathf.Abs(movementSpeed);
+        else
+            movementSpeed = Mathf.Abs(movementSpeed);
+    }
+
+    public void OnButtonUp()
+    {
+        ballMove = false;
+    }
+}
